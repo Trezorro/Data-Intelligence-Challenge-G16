c89329c78fb9cb61cbed38cb33d93fbc5a9c49fb
Yvan Satyawan <y_satyawan@hotmail.com>
2022-05-18 12:59:28 +0200

    Revert "PEP 8 Compliance and some optimizations"
    This reverts commit be183a2d8e23ca646c17d9792af489500f506234.

--- Discrete-Simulations/environment.py
+++ Discrete-Simulations/environment.py
@@ -1,64 +1,40 @@
 from typing import Dict, Tuple
 
-
 import numpy as np
 import random
 
 
 class RobotBase:
-    def __init__(self, grid, pos, orientation, p_move=0, battery_drain_p=1,
-                 battery_drain_lam=0, vision=1):
-        """Base class for all robots.
-
-        Args:
-            grid: The grid the robot will move on.
-            pos: Current position as a tuple (y, x).
-            orientation: Current orientation of the robot. One of "n", "e", "s",
-                "w".
-            p_move: Probability of robot performing a random move instead of
-                listening to a given command.
-            battery_drain_p: Probability of a battery drain event happening at
-                each move.
-            battery_drain_lam: Amount (lambda) of battery drain (X) in
-                X ~ Exponential(lambda)
-            vision: Number of tiles in each of the 4 directions included in the
-                robots vision.
-        """
+    def __init__(self, grid, pos, orientation, p_move=0, battery_drain_p=1, battery_drain_lam=0, vision=1):
         if grid.cells[pos[0], pos[1]] != 1:
             raise ValueError
 
-        # Current orientation of the robot, one of 'n', 'e', 's', 'w'.
-        self.orientation: str = orientation
-        # Grid associated numbers of various robot orientations
-        self.orients = {'e': -3, 's': -4, 'w': -5, 'n': -6}
+        self.orientation: str = orientation  # Current orientation of the robot, one of 'n', 'e', 's', 'w'.
+        self.orients = {'e': -3, 's': -4, 'w': -5, 'n': -6}  # Grid associated numbers of various robot orientations
         self.dirs = {'e': (0, 1), 's': (1, 0), 'w': (0, -1), 'n': (-1, 0)}
         self.dirs_inv = {(0, 1): 'e', (1, 0): 's', (0, -1): 'w', (-1, 0): 'n'}
 
-        self.pos: Tuple[int, int] = pos
-        self.grid: Grid = grid
-        self.p_move = p_move
-        self.battery_drain_p = battery_drain_p
-        self.battery_drain_lam = battery_drain_lam
-        self.vision = vision
+        self.pos: Tuple[int, int] = pos  # Position of the robot on the grid, tuple (y, x)
+        self.grid: Grid = grid  # Instance of Grid class, current playing field.
+        self.p_move = p_move  # Probability of robot performing a random move instead of listening to a given command
+        self.battery_drain_p = battery_drain_p  # Probability of a battery drain event happening at each move.
+        self.battery_drain_lam = battery_drain_lam  # Amount (lambda) of battery drain (X) in X ~ Exponential(lambda)
+        self.vision = vision  # Number of tiles in each of the 4 directions included in the robots vision.
 
         self.grid.cells[pos] = self.orients[self.orientation]
-        # historic x and y coordinates of the robot [[y1, y2,...], [x1, x2,...]]
-        self.history = [[pos[0]], [pos[1]]]
+        self.history = [[pos[0]], [pos[1]]]  # historic x and y coordinates of the robot [[y1, y2,...], [x1, x2,...]]
         self.battery_lvl = 100  # Current battery level (in %)
         self.alive = True  # Indicator of whether the robot is alive
 
-        # Array to store debug values for each tile
-        self.debug_values = np.zeros_like(grid.cells)
+        self.debug_values = np.zeros_like(grid.cells)  # Array to store debug values for each tile
         self.show_debug_values = False
 
     def possible_tiles_after_move(self) -> Dict[Tuple[int, int], int]:
-        """Returns the values of squares the robot can see from its current
-        position.
+        """Returns the values of squares the robot can see from its current position.
 
         Returns:
-            Dictionary containing the moves relative to the robots current
-            position that the robot can do (as keys) and the value of the
-            resulting square (values)
+            Dictionary containing the moves relative to the robots current position that the robot can do (as keys)
+            and the value of the resulting square (values)
 
             example:
             {
@@ -66,15 +42,11 @@ class RobotBase:
             ( 1, 0):  0,    Clean square
             ( 0, 1): -1,    Wall (you will not move if you do this action)
             (-1, 0): -2,    Obstacle (you will not move if you do this action)
-            ( 0,-2):  2,    Goal (it is not possible to move here in one go,
-                                  it is 2 squares away)
-            ( 2, 0):  0,         (it is not possible to move here in one go, it
-                                  is 2 squares away)
-            ( 0, 2):  0,         (it is not possible to move here in one go, it
-                                  is 2 squares away)
-            (-2, 0):  0          (it is not possible to move here in one go, it
-                                  is 2 squares away)
-        }
+            ( 0,-2):  2,    Goal                (it is not possible to move here in one go, it is 2 squares away)
+            ( 2, 0):  0,                        (it is not possible to move here in one go, it is 2 squares away)
+            ( 0, 2):  0,                        (it is not possible to move here in one go, it is 2 squares away)
+            (-2, 0):  0                         (it is not possible to move here in one go, it is 2 squares away)
+            }
         """
 
         # Obtain possible movement directions
@@ -89,25 +61,24 @@ class RobotBase:
             # Loop over possible moves
             for move in moves:
                 # Get square that bot would land on after move.
-                to_check = (np.array(self.pos) + (np.array(move) * (i + 1)))
+                to_check = tuple(np.array(self.pos) + (np.array(move) * (i + 1)))
 
                 # Check whether the square is inside the playing field.
-                if (self.grid.cells.shape[0] > to_check[0] >= 0
-                        and self.grid.cells.shape[1] > to_check[1] >= 0):
-                    # Store move with corresponding grid value that would be
-                    # obtained.
-                    data[(np.array(move) * (i + 1))] = self.grid.cells[to_check]
+                if self.grid.cells.shape[0] > to_check[0] >= 0 and self.grid.cells.shape[1] > to_check[1] >= 0:
+                    # Store move with corresponding grid value that would be obtained.
+                    data[tuple(np.array(move) * (i + 1))] = self.grid.cells[to_check]
+
         return data
 
     def has_visited(self, x: int, y: int) -> bool:
-        """ Function returns whether bot has visited a given coordinate.
+        """ Function returns whether bot has visited a given coordinate combination (x, y)
 
         Args:
             x: the respective x coordinate.
             y: the respective y coordinate.
 
         Returns:
-            Whether the robot has visited the coordinate. True if it has.
+            boolean whether the robot has visited the coordinate.
         """
         for i in range(len(self.history[0])):
             if self.history[0][i] == y and self.history[1][i] == x:
@@ -118,29 +89,16 @@ class RobotBase:
     def move(self) -> Tuple[bool, bool]:
         """ Function that moves the robot.
 
-        This function simulates the movement of the robot. It will try to move
-        forwards in the current direction it is facing. The battery is drained
-        with probability `self.battery_drain_p`. A move in a random direction is
-        done with probability `self.p_move`.
+        This function simulates the movement of the robot. It will try to move forwards in the current direction it is
+        facing. The battery is drained with probability `self.battery_drain_p`. A move in a random direction is done
+        with probability `self.p_move`.
 
         Returns:
-            first boolean indicates
+            first boolean indicates whether
                 True: the bot has moved successfully.
                 False: the bot did not move or the bot moved and died.
-            second boolean indicates whether the battery was drained during the
-            move.
+            second boolean indicates whether the battery was drained during the move
         """
-        def move_and_remember_or_die():
-            """Used in both branches of movement so refactored here. """
-            self.pos = new_pos
-            self.history[0].append(self.pos[0])
-            self.history[1].append(self.pos[1])
-
-            # If moved to death square, then the bot dies.
-            if tile_after_move == 3:
-                self.alive = False
-                return False, do_battery_drain
-            return True, do_battery_drain
 
         # Can't move if we're dead now, can we?
         if not self.alive:
@@ -150,10 +108,9 @@ class RobotBase:
         random_move = np.random.binomial(1, self.p_move)
 
         # Decide whether this move will drain the battery
-        do_battery_drain = (np.random.binomial(1, self.battery_drain_p) == 1)
+        do_battery_drain: bool = (np.random.binomial(1, self.battery_drain_p) == 1)
 
-        # If battery should be drained, drain the battery according to
-        # exponential drain
+        # If battery should be drained, drain the battery according to exponential drain
         if do_battery_drain and self.battery_lvl > 0:
             self.battery_lvl -= np.random.exponential(self.battery_drain_lam)
 
@@ -164,22 +121,26 @@ class RobotBase:
 
         # If random move, execute a random move
         if random_move == 1:
-            # Get possible moves, choose a random one and calculate the square
-            # the robot would supposedly end up on.
+            # Get possible moves, choose a random one and calculate the square the robot would supposedly end up on.
             moves = self.possible_tiles_after_move()
-            random_move = random.choice([m for m in moves if moves[m] >= 0])
+            random_move = random.choice([move for move in moves if moves[move] >= 0])
             new_pos = tuple(np.array(self.pos) + random_move)
 
-            # If the bot can move to that square, move the bot and adapt the
-            # grid.
+            # If the bot can move to that square, move the bot and adapt the grid.
             if self.grid.cells[new_pos] >= 0:
-                dirs_keys = list(self.dirs.keys())
-                random_move_index = list(self.dirs.values()).index(random_move)
-                new_orient = dirs_keys[random_move_index]
+                new_orient = list(self.dirs.keys())[list(self.dirs.values()).index(random_move)]
                 tile_after_move = self.grid.cells[new_pos]
                 self.grid.cells[self.pos] = 0
                 self.grid.cells[new_pos] = self.orients[new_orient]
-                return move_and_remember_or_die()
+                self.pos = new_pos
+                self.history[0].append(self.pos[0])
+                self.history[1].append(self.pos[1])
+
+                # If moved to death square, then the bot dies.
+                if tile_after_move == 3:
+                    self.alive = False
+                    return False, do_battery_drain
+                return True, do_battery_drain
 
             # If we cannot move, just stand still and leave the bot
             else:
@@ -190,33 +151,41 @@ class RobotBase:
             # Calculate the new position that the bot will supposedly end on.
             new_pos = tuple(np.array(self.pos) + self.dirs[self.orientation])
 
-            # If the bot can move to that square, move the bot and adapt the
-            # grid.
+            # If the bot can move to that square, move the bot and adapt the grid.
             if self.grid.cells[new_pos] >= 0:
                 tile_after_move = self.grid.cells[new_pos]
                 self.grid.cells[self.pos] = 0
                 self.grid.cells[new_pos] = self.orients[self.orientation]
-                return move_and_remember_or_die()
+                self.pos = new_pos
+                self.history[0].append(self.pos[0])
+                self.history[1].append(self.pos[1])
+
+                # If moved to death square, then the bot dies.
+                if tile_after_move == 3:
+                    self.alive = False
+                    return False, do_battery_drain
+                return True, do_battery_drain
 
             # If we cannot move, just stand still and leave the bot
             else:
                 return False, do_battery_drain
 
     def rotate(self, direction: str) -> None:
-        """ Rotates the robot in a given direction either left 'l' or right 'r'.
+        """ Rotates the robot in a given direction either left ('l') or right ('r')
 
-        This function rotates the robot either to the left or right based on the
-        given parameter value 'l' or 'r', respectively. The function adapts
-        Robot object orientation attribute and the grid value that the robot is
+        This function rotates the robot either to the left or right based on the given parameter value 'l' or 'r',
+        respectively. The function adapts Robot object orientation attribute and the grid value that the robot is
         currently positioned on.
+
+        Returns:
+            None
         """
 
         # # == Drain the battery
         # # Decide whether this move will drain the battery
         # do_battery_drain = np.random.binomial(1, self.battery_drain_p)
         #
-        # # If battery should be drained, drain the battery according to
-        # exponential drain
+        # # If battery should be drained, drain the battery according to exponential drain
         # if do_battery_drain == 1 and self.battery_lvl > 0:
         #     self.battery_lvl -= np.random.exponential(self.battery_drain_lam)
 
@@ -224,8 +193,7 @@ class RobotBase:
         # Get index of orientation from list ['n', 'e', 's', 'w']
         current = list(self.orients.keys()).index(self.orientation)
 
-        # Get new orientation based on index of current orientation, rotating
-        # direction and list of orientations
+        # Get new orientation based on index of current orientation, rotating direction and list of orientations
         if direction == 'r':
             self.orientation = list(self.orients.keys())[(current + 1) % 4]
         elif direction == 'l':
@@ -298,7 +266,7 @@ class Grid:
         return not (1 in self.cells)
 
     def put_obstacle(self, x0, x1, y0, y1, from_edge=1) -> None:
-        """ Builds an obstacle on the grid starting on xy0 and ending at xy1
+        """ Builds an obstacle on the grid starting on (x0,y0) and ending at (x1,y1)
         """
         self.cells[
                 max(y0, from_edge): min(y1 + 1, self.n_rows - from_edge),
@@ -320,8 +288,7 @@ class Grid:
 
 def generate_grid(n_cols, n_rows):
     # Placeholder function used to generate a grid.
-    # Select an empty grid file in the user interface and add code her to
-    # automatically fill it.
+    # Select an empty grid file in the user interface and add code her to automatically fill it.
     # Look at grid_generator.py for inspiration.
     grid = Grid(n_cols, n_rows)
     return grid
