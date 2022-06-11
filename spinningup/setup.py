ef6e692f43b13a4578e380dd2a330f21461a861f
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2022-06-11 13:37:19 +0200

    changing install and inserting spinning up

---
+++ spinningup/setup.py
@@ -0,0 +1,32 @@
+from os.path import join, dirname, realpath
+from setuptools import setup
+import sys
+
+assert sys.version_info.major == 3 and sys.version_info.minor >= 6, \
+    "The Spinning Up repo is designed to work with Python 3.6 and greater." \
+    + "Please install it before proceeding."
+
+with open(join("spinup", "version.py")) as version_file:
+    exec(version_file.read())
+
+setup(
+    name='spinup',
+    py_modules=['spinup'],
+    install_requires=[
+        'cloudpickle==1.2.1',
+        'ipython',
+        'joblib',
+        'matplotlib',
+        'mpi4py',
+        'numpy',
+        'pandas',
+        'pytest',
+        'psutil',
+        'scipy',
+        'seaborn',
+        'torch',
+        'tqdm'
+    ],
+    description="Teaching tools for introducing people to deep RL.",
+    author="Joshua Achiam",
+)
