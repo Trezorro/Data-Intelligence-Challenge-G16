1d99cf32f5f2c08df3b8367bb58ad6b911d42351
Milan Tresoor <milan.tresoor@prodrive-technologies.com>
2022-06-15 17:07:55 +0200

    Fix setup steps and documentation

--- spinningup/setup.py
+++ spinningup/setup.py
@@ -14,4 +14,19 @@ setup(
     py_modules=['spinup'],
     description="Teaching tools for introducing people to deep RL.",
     author="Joshua Achiam",
+    install_requires=[
+        'cloudpickle==1.2.1',
+        'ipython',
+        'joblib',
+        'matplotlib',
+        'mpi4py',
+        'numpy',
+        'pandas',
+        'pytest',
+        'psutil',
+        'scipy',
+        'seaborn',
+        'torch',
+        'tqdm'
+    ],
 )
